ee8872e804a0e0b0c769222b8285d37f293d856f
Federico-Maiocchi <maiocchifederico@gmail.com>
2024-05-02 13:31:37 +0200

    ADD: DisdiciPosti in Program

--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -13,15 +13,17 @@ namespace csharp_gestore_eventi
 
             //Chiedere all'utente il titolo dell'evento
             Console.WriteLine("Aggiungi un Evento");
-            Console.WriteLine("Inserisci il Titolo");
+            Console.WriteLine("");
+
+            Console.Write("Inserisci il nome dell'evento: ");
             string titolo = Console.ReadLine();
 
             //Chiedere la data dell'evento
-            Console.WriteLine("Inserisci una data giorno/mese/anno");
-            DateTime data = DateTime.ParseExact(Console.ReadLine(), "dd/mm/yyyy", null);
+            Console.Write("Inserisci la data dell'vento una data (giorno/mese/anno): ");
+            DateTime data = DateTime.Parse(Console.ReadLine());
 
             //Chiedere di indicare la capienza massima di posti dell'evento 
-            Console.WriteLine("Inserisci la capienza massima di posti");
+            Console.Write("Inserisci posti totali:");
             int capienzaMassima = int.Parse(Console.ReadLine());
 
             //Creazione dell'evento
@@ -48,7 +50,7 @@ namespace csharp_gestore_eventi
                     continue;
                 }
 
-                //Richiesta conferma prenotazioni posti
+                //Richiesta numero prenotazioni posti
                 Console.WriteLine("Quanti posti vuoi prenotare?");
                 int postiDaPrenotare = int.Parse(Console.ReadLine());
 
@@ -58,8 +60,10 @@ namespace csharp_gestore_eventi
                     //Prenota i posti dentro nuovoEvento
                     //indicando il messaggio di conferma prenotazione / il numero di posti prenotati / numero di posti disponibili
                     nuovoEvento.PrenotaPosti(postiDaPrenotare);
-                    Console.WriteLine($"Prenotazione confermata. posti prenotati {nuovoEvento.PrenotaPosti}, " +
-                        $"posti disponibili {nuovoEvento.CapienzaMassimaEvento - nuovoEvento.NumeroPostiPrenotati}");
+
+                    //3.       Stampare a video il numero di posti prenotati e quelli disponibili.
+                    Console.WriteLine($"Prenotazione confermata. posti prenotati {postiDaPrenotare}");
+                    Console.WriteLine($"posti disponibili {nuovoEvento.CapienzaMassimaEvento - nuovoEvento.NumeroPostiPrenotati}");
                 } 
                 catch (Exception ex) 
                 {
@@ -69,9 +73,50 @@ namespace csharp_gestore_eventi
                
             }
 
-            //3.Stampare a video il numero di posti prenotati e quelli disponibili.
             //4.Ora chiedere all’utente fintanto che lo desidera, se e quanti posti vuole disdire. 
-            //  Ogni volta che disdice dei posti, stampare i posti residui e quelli prenotati. 
+            //  Ogni volta che disdice dei posti, stampare i posti residui e quelli prenotati.
+            while (true)
+            {
+                //Richiesta all'utente se vuole disdire i posti prenotati
+                Console.WriteLine("Vuoi disdire dei posti? Si/No");
+                string disdiciPrenotazione = Console.ReadLine().ToLower();
+
+                //Controllo se risponde no, allora chiude il programma
+                if (disdiciPrenotazione == "no")
+                {
+                    break;
+                }
+
+                //Controllo se risponde si allora passa alla fase successiva
+                if (disdiciPrenotazione != "si")
+                {
+                    Console.WriteLine("Risposta non valida. Inserisci SI oppure NO");
+                    continue;
+                }
+
+                //Richiesta numero disdetta posti
+                Console.WriteLine("Quanti posti vuoi disdire?");
+                int postiDaDisdire = int.Parse(Console.ReadLine());
+
+
+                try
+                {
+                    //Disdici i posti nel nuovoEvento
+                    nuovoEvento.DisdiciPosti(postiDaDisdire);
+                    Console.WriteLine($"Rimossi posti prenotati con successo. Posti disdetti {postiDaDisdire}");
+                    Console.WriteLine($"posti disponibili {nuovoEvento.CapienzaMassimaEvento - nuovoEvento.NumeroPostiPrenotati}");
+                    Console.WriteLine($"posti prenotati {nuovoEvento.NumeroPostiPrenotati}");
+                }
+                catch (Exception ex)
+                {
+                    //In caso di errore, cattura l'eccezione e stampa l'errore
+                    Console.WriteLine($"Errore posti disdetti: {ex.Message}");
+                }
+
+            }
+
+            
+             
 
 
 
